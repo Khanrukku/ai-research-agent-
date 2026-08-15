87e9f1137295d4bb8387da7aa21dd96bba6bef5f
Rukaiya Khan <133051015+Khanrukku@users.noreply.github.com>
2026-08-15 14:36:07 +0530

    Delete infra/aws directory

--- infra/aws/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ecr_repository_url" { value = aws_ecr_repository.agent.repository_url }
-output "ecs_cluster" { value = aws_ecs_cluster.agent.name }
-output "s3_reports_bucket" { value = aws_s3_bucket.reports.bucket }
-output "log_group" { value = aws_cloudwatch_log_group.agent.name }
